54756dcadc7e9fc5b587b82d1bc8256e48724d5c
mlunceford <mark7lunceford@gmail.com>
2019-10-29 12:16:07 -0600

    done

--- src/store.js
+++ src/store.js
@@ -1,9 +1,12 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
 import promiseMiddleware from 'redux-promise-middleware'
-import reducer from './ducks/budgetReducer'
+import budgetReducer from './ducks/budgetReducer'
+import userReducer from './ducks/userReducer'
+
 
 const rootReducer = combineReducers({
-    budget: budgetReducer
+    budget: budgetReducer, // how to grab this is this.props.budget
+    user: userReducer
 })
 
 export default createStore(rootReducer, applyMiddleware(promiseMiddleware))
